4dbd48f4af3783e5e535eafc64fcc2b7775b6ea0
Alex Richards <alexchristopherrichards@gmail.com>
2022-05-28 12:21:29 -0500

    navigation + setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Home from './pages/Home';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/navbar/topbar/Topbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../../../styles/navigation/Topbar.module.scss';
+
+export default function Topbar() {
+  return (
+    <div className={styles.container}>Topbar</div>
+  )
+}
