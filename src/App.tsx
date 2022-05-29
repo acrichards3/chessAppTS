5b101dde0358cfc893ea9bc3a6af3d225a49a680
Alex Richards <alexchristopherrichards@gmail.com>
2022-05-28 20:44:28 -0500

    topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Home from './pages/Home';
 import Navbar from './components/navbar/Navbar';
+import styles from './styles/app/App.module.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+    <div className={styles.app}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/components/navbar/topbar/Topbar.tsx
+++ src/components/navbar/topbar/Topbar.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from '../../../styles/navigation/Topbar.module.scss';
+import { Icon } from '@blueprintjs/core';
 
 export default function Topbar() {
   return (
-    <div className={styles.container}>Topbar</div>
+    <div className={styles.container}>
+        <div className={styles.title}>ChessFront</div>
+        <div className={styles.links}>
+            <Icon icon='shield' size={22} className={styles.link} />
+            <Icon icon='document' size={22} className={styles.link} />
+            <Icon icon='lightbulb' size={22} className={styles.link} />
+        </div>
+    </div>
   )
 }
