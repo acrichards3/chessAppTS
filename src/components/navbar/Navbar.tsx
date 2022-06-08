c0601140bca9039ab551e5e9a3b34b343e0310ff
Alex Richards <alexchristopherrichards@gmail.com>
2022-06-08 15:15:55 -0400

    mobile nav improvements

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Topbar from './topbar/Topbar';
-import MobileNav from './mobile/mobileNav';
+import MobileNav from './mobile/MobileNav';
 
 export default function Navbar() {
   return (
     <>
-      <Topbar />
       <MobileNav />
     </>
   );
--- src/components/navbar/mobile/MobileNav.tsx
+++ src/components/navbar/mobile/MobileNav.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Icon } from '@blueprintjs/core';
+import styles from '../../../styles/navigation/MobileNav.module.scss';
 
 export default function MobileNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuOpen = (isOpen: boolean) => {
+    if (isOpen) {
+      return (
+        <div className="div">
+          testing
+        </div>
+      )
+    } else {
+      return (
+        <div className="div">
+          testing2
+        </div>
+      )
+    }
+  }
+
   return (
-    <div>mobileNav</div>
+    <div className={styles.container}>
+      <div className={styles.title}>ChessFront</div>
+      <div className={styles.menu}>
+        {menuOpen(isOpen)}
+      </div>
+    </div>
   )
 }
