2d5e44462e51fa66aa8ec6340fae72af1b993a77
Alex Richards <alexchristopherrichards@gmail.com>
2022-06-09 01:01:07 -0400

    fully functional mobile friendly navigation

--- src/components/navbar/LINKS.ts
+++ src/components/navbar/LINKS.ts
@@ -13,6 +13,12 @@ interface Icon {
   title: string;
 }
 
+interface Mobile {
+  id: number;
+  url: string;
+  title: string;
+}
+
 export const LINKS: Link[] = [
   {
     id: 0,
@@ -48,6 +54,39 @@ export const ICONS: Icon[] = [
     id: 5,
     url: '/faq',
     icon: 'lightbulb',
+    title: 'FAQ',
+  },
+];
+
+export const MOBILE: Mobile[] = [
+  {
+    id: 0,
+    url: '/',
+    title: 'Dashboard',
+  },
+  {
+    id: 1,
+    url: '/stats',
+    title: 'Stats',
+  },
+  {
+    id: 2,
+    url: '/rank',
+    title: 'Rank',
+  },
+  {
+    id: 3,
+    url: '/privacy-policy',
+    title: 'Privacy Policy',
+  },
+  {
+    id: 4,
+    url: '/terms-and-conditions',
+    title: 'Terms & Conditions',
+  },
+  {
+    id: 5,
+    url: '/faq',
     title: 'FAQ',
   },
 ];
--- src/components/navbar/mobile/MobileNav.tsx
+++ src/components/navbar/mobile/MobileNav.tsx
@@ -1,17 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Icon } from '@blueprintjs/core';
 import { Link } from 'react-router-dom';
+import { MOBILE } from '../LINKS';
 import styles from '../../../styles/navigation/MobileNav.module.scss';
 
 export default function MobileNav() {
   const [isOpen, setIsOpen] = useState(true);
-  
+  const [isActive, setActive] = useState(0);
+
   const menuOpen = (isOpen: boolean) => {
     if (isOpen) {
       return (
-        <button onClick={() => setIsOpen(false)} className={styles.menuBtn}>
-          <Icon icon="menu-closed" size={30} />
-        </button>
+        <div className={styles.openMenu}>
+          <div>
+            <button onClick={() => setIsOpen(false)} className={styles.menuBtn}>
+              <Icon icon="menu-closed" size={30} />
+            </button>
+          </div>
+        </div>
       );
     } else {
       return (
@@ -22,18 +28,48 @@ export default function MobileNav() {
     }
   };
 
+  const currentlyOpen = (isOpen: boolean) => {
+    if (isOpen) {
+      return (
+        <div className={styles.menuOpen}>
+          {MOBILE.map((link) => {
+            return (
+              <Link
+                to={link.url}
+                key={link.id}
+                style={{ textDecoration: 'none' }}
+                className={isActive === link.id ? styles.active : styles.link}
+                onClick={() => setActive(link.id)}
+              >
+                {link.title}
+              </Link>
+            );
+          })}
+        </div>
+      );
+    }
+
+    return;
+  }
+
   return (
+    <>
     <div className={styles.container}>
       <div className={styles.title}>
         <Link
           to="/"
           style={{ textDecoration: 'none' }}
           className={styles.title}
+          onClick={() => setIsOpen(false)}
         >
           ChessFront
         </Link>
       </div>
       <div className={styles.menu}>{menuOpen(isOpen)}</div>
     </div>
+    <div>
+      {currentlyOpen(isOpen)}
+    </div>
+    </>
   );
 }
