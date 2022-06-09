3c36d62f4c98a1421f2f43f98492c199988c614a
Alex Richards <alexchristopherrichards@gmail.com>
2022-06-09 00:11:38 -0400

    functional hamburger icon

--- src/components/navbar/mobile/MobileNav.tsx
+++ src/components/navbar/mobile/MobileNav.tsx
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Icon } from '@blueprintjs/core';
+import { Link } from 'react-router-dom';
 import styles from '../../../styles/navigation/MobileNav.module.scss';
 
 export default function MobileNav() {
-  const [isOpen, setIsOpen] = useState(false);
-
+  const [isOpen, setIsOpen] = useState(true);
+  
   const menuOpen = (isOpen: boolean) => {
     if (isOpen) {
       return (
-        <div className="div">
-          testing
-        </div>
-      )
+        <button onClick={() => setIsOpen(false)} className={styles.menuBtn}>
+          <Icon icon="menu-closed" size={30} />
+        </button>
+      );
     } else {
       return (
-        <div className="div">
-          testing2
-        </div>
-      )
+        <button onClick={() => setIsOpen(true)} className={styles.menuBtn}>
+          <Icon icon="menu" size={30} />
+        </button>
+      );
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.title}>ChessFront</div>
-      <div className={styles.menu}>
-        {menuOpen(isOpen)}
+      <div className={styles.title}>
+        <Link
+          to="/"
+          style={{ textDecoration: 'none' }}
+          className={styles.title}
+        >
+          ChessFront
+        </Link>
       </div>
+      <div className={styles.menu}>{menuOpen(isOpen)}</div>
     </div>
-  )
+  );
 }
